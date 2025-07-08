f3eb9fb5b85ca00f592e83f43fa8181cd20c3035
unknown <Student@Camp-2RQMRK3.com>
2025-07-08 15:19:49 -0400

    test leterenocde later

--- src/main/java/dataProcessing/LetterEncode.java
+++ src/main/java/dataProcessing/LetterEncode.java
@@ -1,6 +1,6 @@
 package dataProcessing;
 
-import org.apache.commons.io.IOExceptionList;
+//import org.apache.commons.io.IOExceptionList;
 
 import java.io.*;
 import java.util.Arrays;
@@ -16,20 +16,61 @@ public class LetterEncode {
     );
 
     // For direction letters (e.g., N/S/E/W)
-    public static final Map<Character, Integer> directionMap = Map.of(
-            'N', 1, 'S', 2, 'E', 3, 'W', 4
-    );
+    /* public static final Map<String, Integer> directionMap = Map.ofEntries(
+            Map.entry("E", 1)
+            Map.entry("W", 2)
+            Map.entry("N", 3)
+            Map.entry("S", 4)
+    ); */
 
     public static void main(String[] args_and_other_unnecessary_stuff) {
-        File inputDir = new File("src/main/resources/storms/");
+        // change that for working on mac
+        File inputDir = new File("src/main/resources/storms");
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println("dinputDir exists : " + inputDir.exists());
         File[] files = inputDir.listFiles();
 
         for(File storm: files) {
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter(storm);
+            } catch(Exception e) {e.printStackTrace();}
             String stormName = storm.getName();
             String[] lines = readLines(1, storm);
+            for (int i = 0; i < lines.length; i++) {
+                String[] entries = splitLine(lines[i]);
+                for (int e = 0; i < entries.length; e++) {
+                    String entry = entries[e];
+                    if(entry.contains("N,")) {
+                        entry = directionEncode(entry, 0);
+                    } else if(entry.contains("S,")) {
+                        entry = directionEncode(entry, 180);
+                    } else if(entry.contains("E,")) {
+                        entry = directionEncode(entry, 90);
+                    } else if (entry.contains("W,")) {
+                        entry = directionEncode(entry, 270);
+                    } else {
+                        entry = String.valueOf(systemCodeMap.get(entry.replaceAll(",", ""))) + ",";
+                    }
+                }// iterated thru entries
+                lines[i] = String.join(",", entries);
+            } // iterated thru lines
+            // iterated through all the lines now
+            try {
+                writer.write(String.join("\n", lines));
+                writer.close();
+            } catch(Exception e) {e.printStackTrace();}
+        } // iterated thru storms
+    }
 
+    private static String directionEncode(String entry, int x) {
+        entry = entry.replaceAll("[^A-Za-z,]","");
+        entry = String.valueOf(Double.parseDouble(entry) + x);
+        return entry;
+    }
 
-        }
+    private static String[] splitLine(String line) {
+        return line.split(",");
     }
 
     private static String[] readLines(int skipNum, File fileName) {
@@ -43,6 +84,7 @@ public class LetterEncode {
                 lines.add(line);
             }
             bufferedReader.close();
+            fileReader.close();
         } catch (Exception e) {
             System.out.println("something done did mess up bad line 46 readlines() LetterEncode.java");
             e.printStackTrace();
